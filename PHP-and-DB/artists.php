08ba29ad2755a9a1f9d63a87b01b9a81b006c9e2
jbennett1996 <jbennett96@live.com.au>
2015-04-20 12:51:18 +1000

    Create artists.php
    This is a really rough start and very incomplete; this page, when complete, will connect to the database and allow you to display/remove/change artists as necessary.

---
+++ PHP-and-DB/artists.php
@@ -0,0 +1,17 @@
+<?php
+include ("dbconnect.php")
+?>
+
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Select, Display and Modify Artists and Content</title>
+</head>
+
+<body>
+<?php 
+$name = "SELECT Artist Name FROM artists";
+echo $name;
+?>
+</body>
+</html>
